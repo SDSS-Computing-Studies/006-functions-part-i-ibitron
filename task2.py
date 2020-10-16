4e75ebdbfc308740f4d52898052b251e5d020a65
ibitron <71407016+ibitron@users.noreply.github.com>
2020-10-16 12:39:35 -0700

    Update task2.py

--- task2.py
+++ task2.py
@@ -6,3 +6,15 @@ The input is a list.
 The return value is the largest value in the list
 (2 points)
 """
+def largest(a):
+
+
+    a.sort()
+    num=a[0]
+    homem = a[-1]
+    print(num)
+    print(homem)
+    return (num)
+
+joshList = [175,28,34,420,5,6,7,7,69]
+largest(joshList)
